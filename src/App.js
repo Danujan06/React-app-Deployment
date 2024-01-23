68930ac678e2a1cce4bce140e3eb972ae8fbe9c6
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-24 00:22:31 +0530

    Testing1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-import PlantDetails from "./pages/Plant Details/Plant_Details"
-
 import RoutesConfig from "../src/pages/4_Routing/RoutesConfig";
 import {BrowserRouter as Router } from "react-router-dom";
 
