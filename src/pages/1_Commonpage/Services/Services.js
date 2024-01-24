0723e1943f095fe59b379a95289afe86a59b2912
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-24 22:27:14 +0530

    Heading of the table container Changed

--- src/pages/1_Commonpage/Services/Services.js
+++ src/pages/1_Commonpage/Services/Services.js
@@ -38,32 +38,35 @@ function Services() {
       <div className="pagefive-container row">
         <div className="page-five-content row">
           <div className="page-five-content-left col">
-            <h2 style={{ textAlign: "center", padding: "20px" }}>System Price</h2>
+            <h2 style={{ textAlign: "center", padding: "20px" }}>Overview Of Our System</h2>
             <div className="left-content">
               <img src={Prototype} alt="prototype" id="page-five-prototype-image" />
             </div>
             <div className="price-button-container">
+              <h3 style={{ textAlign: "center"/*, padding: "20px"*/ }}>Price :</h3>
               <button id="price-button">Rs 40,000.00</button>
             </div>
           </div>
           <div className="page-five-content-right col">
             <h2 style={{ textAlign: "center", padding: "20px" }}>Available Plants</h2>
-            <table className="table border shadow">
-              <thead>
-                <tr>
-                  <th scope="col">No</th>
-                  <th scope="col">Plant Name</th>
-                </tr>
-              </thead>
-              <tbody className="table-group-divider">
-                {plants.map((plant, index) => (
-                  <tr key={plant.id}>
-                    <td scope="row">{index + 1}</td>
-                    <td>{plant.name}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <div className = "table-container">
+                <table className="table border shadow">
+                  <thead>
+                    <tr>
+                      <th scope="col">No</th>
+                      <th scope="col">Plant Name</th>
+                    </tr>
+                  </thead>
+                  <tbody className="table-group-divider">
+                    {plants.map((plant, index) => (
+                      <tr key={plant.id}>
+                        <td scope="row">{index + 1}</td>
+                        <td>{plant.name}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+            </div>
           </div>
         </div>
       </div>
